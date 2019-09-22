4f5998fd650e8cb74c7d9ac7e230de2abe4e0dc3
bertRC <sithalbert@ya.ru>
2019-09-22 15:59:56 +0300

    Реализован класс Tariff

---
+++ src/main/java/Tariff.java
@@ -0,0 +1,17 @@
+public class Tariff {
+    long id;
+    String name;
+    int price;
+    boolean isPerMonth;
+    boolean isSmart;
+
+    int gigabytes;
+    boolean isUnlimitedSocialNetworks;
+    String internetAdditionalfeatures;
+
+    int minutes;
+    boolean isUnlimitedToTele2;
+    int smsCount;
+
+    boolean isCustomizable;
+}
